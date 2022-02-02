b25cf7ec0102bab25cb52d75ce160f975b713500
vladicK20 <vladvishnevskiy2018@gmail.com>
2022-02-03 00:51:23 +0200

    .

---
+++ Lesson 1/classwork (2.02.22)/main.js
@@ -0,0 +1,48 @@
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let result = array[0] + array[1] + array[2] + array[3] + array[4] + array[5] + array[6] + array[7] + array[8] + array[9]
+console.log(result)
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+
+let book1 = {
+    name: "Witcher",
+    pages: 368,
+    genre: "Fantasy"
+}
+console.log(book1)
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+let book2 = {
+    name: "Witcher",
+    pages: 368,
+    genre: "Fantasy",
+    author: "Anjey Sapkowski"
+}
+console.log(book2)
+// 4. Створити масив об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+let books =
+    [
+        book1 =
+            {name: "Witcher", pages: 368, genre: "Fantasy", author: "Anjey Sapkowski"},
+        book2 =
+            {name: "Witcher", pages: 368, genre: "Fantasy", author: "Anjey Sapkowski"}
+    ]
+console.log(books[0], books[1])
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
+let height = 23
+let width = 10
+let s = `Площа прямокутника = ${height * width}`
+console.log(s)
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+let heightC = 10
+let dC = 4/2
+const pi = 3.14
+let v = `Об'єм циліндра = ${pi * dC**2 * heightC}`
+console.log(v)
+// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+let n = 3
+let m = 4
+let k = `Гіпотенуза = ${n**2 + m**2}`
+console.log(k)
--- Lesson 1/homework (31.01.22)/main.js
+++ Lesson 1/homework (31.01.22)/main.js
@@ -1,15 +1,100 @@
 // - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
 //     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let a = "hello"
+// let b = "owu"
+// let c = "com"
+// let d = "ua"
+// let e = 1
+// let f = 10
+// let g = -999
+// let h = 123
+// let i = 3.14
+// let j = 2.7
+// let k = 16
+// let l = true
+// let m = false
+// console.log(a, b, c, d, e, f, g, h, i, j, k, l, m)
+// alert(a)
+// alert(b)
+// alert(c)
+// alert(d)
+// alert(e)
+// alert(f)
+// alert(g)
+// alert(h)
+// alert(i)
+// alert(j)
+// alert(k)
+// alert(l)
+// alert(m)
+// document.write(a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g + " " + h + " " + i + " " + j + " " + k + " " + l + " " + m)
+
 // - Переприсвоїти кожній змінній з завдання значення на довільне.
 //     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let a = "hello"
+// a = "one"
+// let b = "owu"
+// b = "two"
+// let c = "com"
+// c = "three"
+// let d = "ua"
+// d = "com"
+// let e = 1
+// e = 2
+// let f = 10
+// f = 11
+// let g = -999
+// g = 999
+// let h = 123
+// h = 1234
+// let i = 3.14
+// i = 3
+// let j = 2.7
+// j = 2
+// let k = 16
+// k = 15
+// let l = true
+// l = false
+// let m = false
+// m = true
+// console.log(a, b, c, d, e, f, g, h, i, j, k, l, m)
+// alert(a)
+// alert(b)
+// alert(c)
+// alert(d)
+// alert(e)
+// alert(f)
+// alert(g)
+// alert(h)
+// alert(i)
+// alert(j)
+// alert(k)
+// alert(l)
+// alert(m)
+// document.write(a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g + " " + h + " " + i + " " + j + " " + k + " " + l + " " + m)
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// let lastName = "Вишневський"
+// let firstName = "Владислав"
+// let middleName = "Юрійович"
+// let fullName = lastName + " " + firstName + " " + middleName
+// console.log(fullName)
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
-//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// let lastName = prompt("What is your lastname?", "Vyshnevskyi")
+// let firstName = prompt("What is your name?", "Vladyslav")
+// let age = prompt("How old are you?", 20)
+// //     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// alert(`"Вітаю ${lastName} ${firstName}. Тобі ${age} років.`)
 // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
 //     let a = 100; let b = '100'; let c = true;
+// let a = 100;
+// console.log(a, typeof a)
+// let b = '100';
+// console.log(b, typeof b)
+// let c = true;
+// console.log(c, typeof c)
 // - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
 //     В однакових виразах не використовувати однакові оператори!!!
-//     5 ? 6 -> true
+// 5 ? 6 -> true
 // 5 ? 6 -> false
 // 5 ? 6 -> false
 // 5 ? 6 -> false
@@ -20,17 +105,28 @@
 // 10 ? 10 -> false
 // 123 ? '123' -> false
 // 123 ? '123' -> true
+// let n1 = 5 < 6
+// let n2 = 5 > 6
+// let n3 = 5 === 6
+// let n4 = 5 >= 6
+// let n5 = 10 === 10
+// let n6 = 10 == 10
+// let n7 = 10 !== 10
+// let n8 = 10 < 10
+// let n9 = 10 > 10
+// let n10 = 123 === "123"
+// let n11 = 123 !== "123"
 //
+// console.log(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11)
+
 // - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
-//     let str = "20";
+// let str = "20";
 // let a = 5;
 // document.write(str + a + "<br/>");
 // document.write(str - a + "<br/>");
 // document.write(str * "2" + "<br/>");
 // document.write(str / 2 + "<br/>");
 
-console.log(100)
-let num = `100`
-console.log(typeof num)
-num = 100
-console.log(typeof num)
+//У вікні браузера буде наступне:
+// Перше значення 5 - тому що цифра + стрінга не додадуться як два числа, а просто з'єднаються як текст
+// Всі інші значення очевидно буду обраховуватись ніби там два числа, оскільки зі всіма іншими діями "20" сприймається як число, і відповідно виводить правильний математичний результат, але не розумію чому саме так.
